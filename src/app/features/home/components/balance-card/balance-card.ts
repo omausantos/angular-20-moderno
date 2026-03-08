1ef6816f7263e0ec30f23458a2197081b8155efa
Mauricio Santos <mauwebjc@gmail.com>
2026-03-07 22:28:05 -0300

    feat(BalanceCard): Regra CSS para para diferença de valor negativo e positivo

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -11,4 +11,13 @@ export class BalanceCard {
   type = input.required<'income' | 'outcome' | 'balance'>();
   label = input.required<string>();
   value = input.required<number>();
+
+  cssClass = computed(() => {
+    if (this.type() === 'income') {
+      return 'income';
+    } else if (this.type() === 'outcome') {
+      return 'outcome';
+    }
+    return this.value() >= 0 ? 'income' : 'outcome';
+  });
 }
