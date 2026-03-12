b3a239019ec23ff7429e1f57bdc8a456bb6161c1
Mauricio Santos <mauwebjc@gmail.com>
2026-03-12 20:09:43 -0300

    feat(Balance): Calculando valores financeiros com Signals

--- src/app/features/home/components/balance/balance.ts
+++ src/app/features/home/components/balance/balance.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BalanceCard } from "./components/balance-card/balance-card";
+import { Component, computed, input } from '@angular/core';
+import { BalanceCard } from './components/balance-card/balance-card';
 
 @Component({
   selector: 'app-balance',
@@ -8,5 +8,19 @@ import { BalanceCard } from "./components/balance-card/balance-card";
   styleUrl: './balance.scss',
 })
 export class Balance {
+  transations = input.required<Array<{ type: 'income' | 'outcome' | string; value: number }>>();
 
+  transationsIncome = computed(() =>
+    this.transations()
+      .filter((t) => t.type === 'income')
+      .reduce((acc, t) => acc + t.value, 0),
+  );
+
+  transationsOutcome = computed(() =>
+    this.transations()
+      .filter((t) => t.type === 'outcome')
+      .reduce((acc, t) => acc + t.value, 0),
+  );
+
+  balance = computed(() => this.transationsIncome() - this.transationsOutcome());
 }
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Balance } from "./components/balance/balance";
+import { Component, signal } from '@angular/core';
+import { Balance } from './components/balance/balance';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,13 @@ import { Balance } from "./components/balance/balance";
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  transations = signal([
+    { type: 'income', value: 1000 },
+    { type: 'outcome', value: 500 },
+    { type: 'income', value: 500 },
+    { type: 'outcome', value: 100 },
+    { type: 'income', value: 500 },
+    { type: 'outcome', value: 500 },
+  ]);
+}
