eaec1128cf4a009fdffa8b409014fb0277298e68
Mauricio Santos <mauwebjc@gmail.com>
2026-04-23 20:24:57 -0300

    feat(Create): Constuindo formulário para adicionar transação (Parte 2)

--- src/app/features/home/pages/create/create.component.ts
+++ src/app/features/home/pages/create/create.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatError, MatFormField, MatLabel, MatPrefix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatFormField, MatInput, MatLabel, MatError, MatPrefix, ReactiveFormsModule, MatButton],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
   form = new FormGroup({
     title: new FormControl('', {
+      validators: [
+        Validators.required,
+      ]
+    }),
+    value: new FormControl('', {
       validators: [
         Validators.required,
       ]
